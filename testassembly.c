aa6808f6aed28010a6dd006daac26c2f6c67bbf9
PsykeDady <psdady@msn.com>
2019-06-07 13:22:00 +0200

    UNROLLING finita e testata con r=2
    TODO: usare r=128 -> poi r=32 -> poi r=8 o 3

--- testassembly.c
+++ testassembly.c
@@ -2,7 +2,7 @@
 #include <xmmintrin.h>
 #include <math.h>
 
-#define D 7
+#define D 23
 
 /* float dist_2_asm(int d, float *x, int xi, float *y, int yi){
         float somma=0, differenza=0;
@@ -24,20 +24,10 @@ float dist(int d, float *x, int xi, float *y, int yi){
 
 int main (int argc, char** args){
 
-    float v1[D]={1.5,-4.2,-1.5,1.0,1,1,0};
-    float *v2=(float*)_mm_malloc(D*sizeof(float),32);
-    int i=0;
-    v2[i++]=1;
-    v2[i++]=1;
-    v2[i++]=-1.5;
-    v2[i++]=1;
-    v2[i++]=0;
-    v2[i++]=1;
-    v2[i++]=1;
-
+    float v1[D]={1.5,-4.2,3,-1.5,1.0,1,1,0,1.5,4,-4.2,-1.5,1.0,1,1,0,1.5,-4.2,-1.5,1.0,1,1,10};
+    float v2[D]={1.5,-4.2,2,-1.5,1.0,1,1,0,1.5,2,-4.2,-1.5,1.0,1,1,0,1.5,-4.2,-1.5,1.0,1,1,0};
     
 
     printf("distanza=%f\n",dist(D,v1,0,v2,0));
 
-    _mm_free(v2);
 }
--- utility.asm
+++ utility.asm
@@ -4,6 +4,25 @@
 
 section .text
 
+%macro dist_2_step 0
+    ;inserimento x in xmm1 (packed)
+    mov      edx, [ebp+16] ; edx=xi
+    add      edx, edi ; edx=xi+i
+    mov      eax, [ebp+12] ; eax= [x]
+    movups   xmm1, [eax+edx*4];xmm1=x[edx]=x + edx
+
+;sottrazione di y in xmm1 (packed)
+    mov     edx, [ebp+24] ; edx=yi
+    add     edx, edi ; edx=yi+i
+    mov     eax, [ebp+20] ; eax= [y]
+    subps  xmm1, [eax+edx*4];xmm1=x[edx]=x + edx
+
+;elevamento al quadrato di xmm1, aggiunta in xmm0 e incremento di i + 4
+    mulps   xmm1,xmm1;xmm1*=xmm1
+    addps   xmm0,xmm1;somma+=xmm1
+    add     edi, 4
+%endmacro
+
 	global dist_2_asm
 
 dist_2_asm:
@@ -27,33 +46,33 @@ dist_2_asm:
     xor     edi, edi ; i = 0
     xorps   xmm0, xmm0 ; somma = 0
     mov     esi, [ebp+8] ; esi = d  
+    sub     esi, 4*2 ; esi = d - p*r
+    inc     esi     ; esi = d - p*r + 1
+
+LOOPU:
+    
+    cmp edi, esi ; i<d-p+1? // da provare se è possibile fare direttamente [ebp+8]-4+1
+    jge ENDU
+
+    dist_2_step 
+    dist_2_step 
+    
+    jmp     LOOPU
+
+ENDU:
+
+    mov     esi, [ebp+8] ; esi = d  
     sub     esi, 4 ; esi = d - p
     inc     esi     ; esi = d - p + 1
 
 LOOPQ:
-    
+
     cmp edi, esi ; i<d-p+1? // da provare se è possibile fare direttamente [ebp+8]-4+1
     jge LOOPR
-
-;inserimento x in xmm1 (packed)
-    mov      edx, [ebp+16] ; edx=xi
-    add      edx, edi ; edx=xi+i
-    mov      eax, [ebp+12] ; eax= [x]
-    movups   xmm1, [eax+edx*4];xmm1=x[edx]=x + edx
-
-;sottrazione di y in xmm1 (packed)
-    mov     edx, [ebp+24] ; edx=yi
-    add     edx, edi ; edx=yi+i
-    mov     eax, [ebp+20] ; eax= [y]
-    subps  xmm1, [eax+edx*4];xmm1=x[edx]=x + edx
-
-;elevamento al quadrato di xmm1, aggiunta in xmm0 e incremento di i + 4
-    mulps   xmm1,xmm1;xmm1*=xmm1
-    addps   xmm0,xmm1;somma+=xmm1
-    add     edi, 4
+ 
+    dist_2_step 
+    
     jmp     LOOPQ
-
-
 
 LOOPR:
     cmp edi, [ebp+8] ; i<d?
@@ -78,6 +97,97 @@ LOOPR:
     jmp LOOPR
 
 END:
+
+    haddps xmm0, xmm0   ; per spostare la somma totale nella prima cella di xmm0
+    haddps xmm0, xmm0   ; idem sopra
+    ;i==d
+    ; somma è già xmm0, e xmm0 è il valore che contiene il return value
+    ; mov eax, [ebp+8] ;vedi riga sotto
+    ; cvtsi2ss xmm0,eax ; decommentare per far vedere che il valore di ritorno esiste
+    movd [ebp-4], xmm0
+    fld dword [ebp-4]
+    pop esi
+    pop edi
+    pop edx
+    pop eax
+    pop ebp
+    
+    ret
+
+global dist_2_asm_vect
+
+dist_2_asm_vect:
+	; float dist_2(int d, float *x, int xi, float *y, int yi){
+    ;     float somma=0, differenza=0;
+    ;     for(int i=0;i<d;i++){
+    ;       differenza=x[xi+i]-y[yi+i];
+    ;       differenza*=differenza;
+    ;       somma+=differenza;
+    ;    }
+    ;    return somma;
+    ; }
+
+    push    ebp ;[ebp+8]= d, [ebp+16]= xi, [ebp+24]= yi, [ebp+12]= x, [ebp+24]= y
+    mov     ebp, esp
+    push    eax
+    push    edx
+    push    edi 
+    push    esi 
+    
+    xor     edi, edi ; i = 0
+    xorps   xmm0, xmm0 ; somma = 0
+    mov     esi, [ebp+8] ; esi = d  
+    sub     esi, 4 ; esi = d - p
+    inc     esi     ; esi = d - p + 1
+
+LOOPQ1:
+    
+    cmp edi, esi ; i<d-p+1? // da provare se è possibile fare direttamente [ebp+8]-4+1
+    jge LOOPR1
+
+;inserimento x in xmm1 (packed)
+    mov      edx, [ebp+16] ; edx=xi
+    add      edx, edi ; edx=xi+i
+    mov      eax, [ebp+12] ; eax= [x]
+    movups   xmm1, [eax+edx*4];xmm1=x[edx]=x + edx
+
+;sottrazione di y in xmm1 (packed)
+    mov     edx, [ebp+24] ; edx=yi
+    add     edx, edi ; edx=yi+i
+    mov     eax, [ebp+20] ; eax= [y]
+    subps  xmm1, [eax+edx*4];xmm1=x[edx]=x + edx
+
+;elevamento al quadrato di xmm1, aggiunta in xmm0 e incremento di i + 4
+    mulps   xmm1,xmm1;xmm1*=xmm1
+    addps   xmm0,xmm1;somma+=xmm1
+    add     edi, 4
+    jmp     LOOPQ1
+
+
+
+LOOPR1:
+    cmp edi, [ebp+8] ; i<d?
+    jge END
+
+; inserimento x in xmm1 (scalar)
+    mov      edx, [ebp+16] ; edx=xi
+    add      edx, edi ; edx=xi+i
+    mov      eax, [ebp+12] ; eax= [x]
+    movss   xmm1, [eax+edx*4];xmm1=x[edx]=x + edx  
+
+;sottrazione di y in xmm1 (scalar)
+    mov     edx, [ebp+24] ; edx=yi
+    add     edx, edi ; edx=yi+i
+    mov     eax, [ebp+20] ; eax= [y]
+    subss  xmm1, [eax+edx*4];xmm1=x[edx]=x + edx 
+
+;elevamento al quadrato di xmm1, aggiunta in xmm0 e incremento di i + 1
+    mulss   xmm1,xmm1;xmm1*=xmm1
+    addss   xmm0,xmm1;somma+=xmm1    
+    inc edi
+    jmp LOOPR1
+
+END1:
 
     haddps xmm0, xmm0   ; per spostare la somma totale nella prima cella di xmm0
     haddps xmm0, xmm0   ; idem sopra
